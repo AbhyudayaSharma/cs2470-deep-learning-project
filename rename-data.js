478e78f59193144d0c125f0b3a198976b7d3b734
Abhyudaya Sharma <11471599+AbhyudayaSharma@users.noreply.github.com>
2023-04-18 19:09:13 -0400

    Fix rename-data.js

--- rename-data.js
+++ rename-data.js
@@ -6,21 +6,37 @@ function main() {
     const trainPath = process.argv[3];
     const testPath = process.argv[4];
 
-    fs.mkdirSync(testPath);
-    fs.mkdirSync(trainPath);
+    fs.mkdirSync(testPath, {recursive: true});
+    fs.mkdirSync(trainPath, {recursive: true});
 
     const countries = fs.readdirSync(dataPath);
     countries.forEach(country => {
         const slug = country.trim().toLowerCase().replace(/\s/g, '_');
 
-        fs.readdirSync(path.join(dataPath, country)).forEach((imgName, i) => {
+        const images = fs.readdirSync(path.join(dataPath, country));
+        let testCount = 0;
+        let trainCount = 0;
+
+        for (let i = 0; i < images.length; i++) {
+            const imgName = images[i];
             const imgPath = path.join(dataPath, country, imgName);
-            const extension = path.extname(imgName)
-            const newPath = path.join(Math.random() > 0.8 ? testPath : trainPath,
-                `${slug}_${i.toString().padStart(5, '0')}${extension}`);
+            const extension = path.extname(imgName);
+            const isTest = Math.random() > 0.8;
+
+            let newPath = '';
+            if (isTest) {
+                newPath = path.join(testPath, `${slug}_${testCount.toString().padStart(5, '0')}${extension}`);
+                testCount++;
+            } else {
+                newPath = path.join(trainPath, `${slug}_${trainCount.toString().padStart(5, '0')}${extension}`);
+                trainCount++;
+            }
+
             console.log(`Renaming ${imgPath} to ${newPath}`);
             // fs.renameSync(imgPath, newPath);
-        });
+        }
+
+        console.log(`${country}: ${trainCount} ${testCount}`);
     });
 }
 
