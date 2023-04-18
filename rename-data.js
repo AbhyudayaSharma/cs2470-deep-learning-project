97268494a241633e97a2630568d9c79e11fe5978
Abhyudaya Sharma <11471599+AbhyudayaSharma@users.noreply.github.com>
2023-04-18 18:44:22 -0400

    No ESM

--- rename-data.js
+++ rename-data.js
@@ -1,25 +1,24 @@
-import {renameSync, readdirSync, mkdirSync} from 'fs';
-import 'path';
-import * as path from "path";
+const fs = require('fs');
+const path = require('path');
 
 function main() {
     const dataPath = process.argv[2];
     const trainPath = process.argv[3];
     const testPath = process.argv[4];
 
-    mkdirSync(testPath);
-    mkdirSync(trainPath);
+    fs.mkdirSync(testPath);
+    fs.mkdirSync(trainPath);
 
-    const countries = readdirSync(dataPath);
+    const countries = fs.readdirSync(dataPath);
     countries.forEach(country => {
         const slug = country.trim().toLowerCase().replaceAll(/\s/, '_');
 
-        readdirSync(path.join(dataPath, country)).forEach((imgName, i) => {
+        fs.readdirSync(path.join(dataPath, country)).forEach((imgName, i) => {
             const imgPath = path.join(dataPath, country, imgName);
             const extension = path.extname(imgName)
             const newPath = path.join(Math.random() > 0.8 ? testPath : trainPath,
                 `${slug}_${i.toString().padStart(3, '0')}.${extension}`);
-            renameSync(imgPath, newPath);
+            fs.renameSync(imgPath, newPath);
         });
     });
 }
