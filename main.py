94085b9f00c6a5377b72e8a421b6915c5b62dcdf
Klimentina Krstevska <kk866@drexel.edu>
2023-04-18 20:46:07 -0400

    adding custom dataiterator and calling the dataloader in main

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import torch
+from torch.utils.data import DataLoader
+
+from preprocess import ImageDataset
 
 
 def main():
     x = torch.rand(5, 3)
     print(x)
 
+    train_dataset = ImageDataset(directory_path='/var/project/train_data')
+    test_dataset = ImageDataset(directory_path='/var/project/test_data')
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=256, shuffle=True)
+    test_dataloader = DataLoader(dataset=test_dataset, batch_size=256, shuffle=True)
+
+    for image_tensor, label in train_dataloader[:10] :
+      print(image_tensor, label)
+
+    for image_tensor, label in test_dataloader[:10] :
+      print(image_tensor, label)
 
 if __name__ == '__main__':
     main()
---
+++ preprocess.py
@@ -0,0 +1,22 @@
+import os
+import torch
+import torchvision
+
+from torch.utils.data import DataLoader
+from torchvision.io.image import ImageReadMode
+
+class ImageDataset(torch.utils.data.IterableDataset):
+
+    def __init__(self, directory_path) -> None:
+        super(ImageDataset).__init__()
+
+        self.image_paths = os.scandir(path=directory_path)
+
+    def __iter__(self) -> None:
+        # get name of next image
+        for path in self.image_paths:
+            # extract label from image name
+            label = path.split(".")[0]
+            label = label[: label.rindex('_')]
+            # return image as tensor and label
+            yield torchvision.io.read_image(path, mode=ImageReadMode.RGB), label
