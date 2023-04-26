aa2e2ea128032bfa46eea8964cdc027c35faf986
Klimentina Krstevska <kk866@drexel.edu>
2023-04-25 20:21:26 -0400

    adding simple model

--- main.py
+++ main.py
@@ -2,27 +2,101 @@ import torch
 from torch.utils.data import DataLoader
 
 from preprocess import ImageDataset
+from model import SimpleConvModel
 
+from sklearn.metrics import classification_report
+from torchvision.transforms import ToTensor
+from torch.optim import Adam
+from torch.nn.functional import one_hot
 
 def main():
     x = torch.rand(5, 3)
     print(x)
 
+    BATCH_SIZE = 256
     train_dataset = ImageDataset(directory_path='/var/project/train_data')
     test_dataset = ImageDataset(directory_path='/var/project/test_data')
 
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE)
+    test_dataloader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE)
 
+    train_steps = len(train_dataloader.dataset) // BATCH_SIZE
+    test_steps = len(test_dataloader.dataset) // BATCH_SIZE
 
-    train_dataloader = DataLoader(dataset=train_dataset, batch_size=256)
-    test_dataloader = DataLoader(dataset=test_dataset, batch_size=256)
+    # for i in range(10):
+    #     image_tensor, label = next(iter(train_dataloader))
+    #     print(image_tensor, label)
+    #
+    # for i in range(10):
+    #     image_tensor, label = next(iter(test_dataloader))
+    #     print(image_tensor, label)
 
-    for i in range(10):
-        image_tensor, label = next(iter(train_dataloader))
-        print(image_tensor, label)
+    model = SimpleConvModel(
+        numChannels=3,
+        classes=43
+    )
+    # initialize our optimizer and loss function
+    opt = Adam(model.parameters(), lr=1e-3)
+    loss_fn = model.loss
 
-    for i in range(10):
-        image_tensor, label = next(iter(test_dataloader))
-        print(image_tensor, label)
+    # initialize a dictionary to store training history
+    H = {
+        "train_loss": [],
+        "train_acc": []
+    }
+
+    # loop over our epochs
+    for e in range(0, 1):
+
+        model.train()
+
+        total_train_loss = 0
+        total_test_loss = 0
+
+        num_correct_train_predictions = 0
+        num_correct_validation_predictions = 0
+
+        for x, y in next(iter(train_dataloader)):
+            y = one_hot(torch.Tensor(map(lambda label: train_dataset.label_map[label], y)), num_classes=43)
+            # (x, y) = (x.to(device), y.to(device)
+
+            # zero out the gradients, perform the backpropagation step,and update the weights
+            opt.zero_grad()
+
+            pred = model(x)
+            loss = loss_fn(pred, y)
+
+            loss.backward()
+            opt.step()
+
+            total_train_loss += loss
+            num_correct_train_predictions += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+        # calculate the average training loss and accuracy
+        avg_train_loss = total_train_loss / train_steps
+        avg_train_acc = num_correct_train_predictions / len(train_dataloader.dataset)
+
+        # update our training history
+        H["train_loss"].append(avg_train_loss)
+        H["train_acc"].append(avg_train_acc)
+        # print the model training and validation information
+        print("[INFO] EPOCH: {}/{}".format(e + 1, EPOCHS))
+        print("Train loss: {:.6f}, Train accuracy: {:.4f}".format(
+            avg_train_loss, avg_train_acc))
+
+    with torch.no_grad():
+        # set the model in evaluation mode
+        model.eval()
+        for x, y in next(iter(test_dataloader)):
+
+            y = one_hot(torch.Tensor(map(lambda label: test_dataset.label_map[label], y)), num_classes=43)
+
+            # send the input to the device
+            #(x, y) = (x.to(device), y.to(device))
+
+            pred = model(x)
+            total_test_loss += loss_fn(pred, y)
+            num_correct_validation_predictions += (pred.argmax(1) == y).type(torch.float).sum().item()
 
 if __name__ == '__main__':
     main()
