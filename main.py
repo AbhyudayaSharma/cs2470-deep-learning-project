e4a254df1734968fce0ffa1e1e775077fe93e15a
Abhyudaya Sharma <11471599+AbhyudayaSharma@users.noreply.github.com>
2023-04-29 11:20:33 -0400

    Top-k accuracy

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ from torch.optim import Adam
 from torch.nn.functional import one_hot
 
 
+def correct_predictions(truth, predictions, top_k=3):
+    count = 0
+    _, top5_catid = torch.topk(predictions, top_k)
+    for i in range(predictions.shape[0]):
+        if truth[i] in top5_catid[i]:
+            count += 1
+
+    return count
+
 def main():
     device = torch.device('cuda')
     x = torch.rand(5, 3)
@@ -55,6 +64,7 @@ def main():
 
         train_steps = 0
         for x, y in iter(train_dataloader):
+            y_original = y
             y = one_hot(
                 torch.Tensor(list(map(lambda label: train_dataset.label_map[label], y))).to(torch.int64),
                 num_classes=43,
@@ -73,9 +83,12 @@ def main():
             opt.step()
 
             total_train_loss += loss
-            num_correct_train_predictions += (
-                (pred.argmax(1) == y.argmax(1)).type(torch.float16).sum().item()
-            )
+
+            accuracy(y_original, pred)
+            # num_correct_train_predictions += (
+            #     (torch.nn.functional.softmax(pred[0], dim=0).argmax(1) == y.argmax(1)).type(torch.float16).sum().item()
+            # )
+            num_correct_train_predictions += correct_predictions(y_original, torch.nn.functional.softmax(pred))
 
             train_steps += 1
             gc.collect()
@@ -103,6 +116,7 @@ def main():
         # set the model in evaluation mode
         model.eval()
         for x, y in iter(test_dataloader):
+            y_original = y
             y = one_hot(
                 torch.Tensor(list(map(lambda label: test_dataset.label_map[label], y))).to(torch.int64),
                 num_classes=43,
@@ -116,9 +130,10 @@ def main():
 
             pred = model(x)
             total_test_loss += loss_fn(pred, y)
-            num_correct_test_predictions += (
-                (pred.argmax(1) == y.argmax(1)).type(torch.float16).sum().item()
-            )
+            num_correct_test_predictions += correct_predictions(y_original, torch.nn.functional.softmax(pred))
+            # num_correct_test_predictions += (
+            #     (torch.nn.functional.softmax(pred[0], dim=0).argmax(1) == y.argmax(1)).type(torch.float16).sum().item()
+            # )
 
             gc.collect()
             break
