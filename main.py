d596710f5020992f8297f7a88887c917221d2d6d
Klimentina Krstevska <kk866@drexel.edu>
2023-04-30 19:08:58 -0400

    break

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def main():
             # increment counter and run garbage collector
             train_steps += 1
             gc.collect()
-            # break
+            break
 
         # calculate the average training loss and accuracy
         avg_train_loss = total_train_loss / train_steps
@@ -162,7 +162,7 @@ def main():
             # increment counter and run garbage collector
             test_steps += 1
             gc.collect()
-            # break
+            break
 
         # calculate the average training loss and accuracy
         avg_test_loss = total_test_loss / test_steps
