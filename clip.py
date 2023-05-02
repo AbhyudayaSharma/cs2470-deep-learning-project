cf3e3185b40c0d5fcf25a62cd60b493f253f800a
Klimentina Krstevska <kk866@drexel.edu>
2023-05-02 11:32:32 -0400

    adding clip module

---
+++ clip.py
@@ -0,0 +1,111 @@
+import os
+import gc
+import torch
+import clip
+from torch.utils.data import DataLoader
+from preprocess import ImageDataset
+from sklearn.linear_model import LogisticRegression
+
+torch.cuda.empty_cache()
+
+def correct_predictions(logits, predictions, top_k=3):
+    count = 0
+
+    _, top5_catid = torch.topk(predictions, top_k)
+    for i in range(logits.shape[0]):
+        if logits[i] in top5_catid[i]:
+            count += 1
+
+    return count
+
+def clip_module():
+
+    # set command line argument
+    BATCH_SIZE = 100
+
+    # define model
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device=device)
+    model = model.to(device)
+    # show model architecture
+    print(model)
+
+    # get datasets
+    train_dataset = ImageDataset(directory_path='/var/project/train_data', clip = preprocess)
+    test_dataset = ImageDataset(directory_path='/var/project/test_data', clip = preprocess)
+
+    # load data
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE)
+    test_dataloader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE)
+
+    # count number of images
+    train_data_count = 25248
+    test_image_count = 6283
+
+    # get training features
+    train_features = []
+    train_labels = []
+    with torch.no_grad():
+
+        train_steps = 0
+        for x, y in iter(train_dataloader):
+            print(f' {train_steps}')
+
+            features = model.encode_image(x.to(device))
+            labels = torch.Tensor(list(map(lambda label: train_dataset.label_map[label], y))).to(torch.int64)
+
+            train_features.append(features)
+            train_labels.append(labels)
+
+            # increment counter and run garbage collector
+            train_steps += 1
+            gc.collect()
+            # break
+
+    # get testing features
+    test_features = []
+    test_labels = []
+    with torch.no_grad():
+
+        test_steps = 0
+        for x, _ in iter(test_dataloader):
+            print(f' {test_steps}')
+
+            features = model.encode_image(x.to(device))
+            labels = torch.Tensor(list(map(lambda label: train_dataset.label_map[label], y))).to(torch.int64)
+
+            test_features.append(features)
+            test_labels.append(labels)
+
+            # increment counter and run garbage collector
+            test_steps += 1
+            gc.collect()
+            # break
+
+    # Perform logistic regression
+    classifier = LogisticRegression(random_state=0, C=0.316, max_iter=1000, verbose=1)
+    classifier.fit(train_features, train_labels)
+
+    # Evaluate using the logistic regression classifier
+    predictions = classifier.predict(test_features)
+
+    classes = test_labels
+    num_correct_test_predictions_top1 = correct_predictions(classes, predictions, top_k=1)
+    num_correct_test_predictions_top2 = correct_predictions(classes, predictions, top_k=2)
+    num_correct_test_predictions_top3 = correct_predictions(classes, predictions, top_k=3)
+    num_correct_test_predictions_top5 = correct_predictions(classes, predictions, top_k=5)
+
+    # calculate the top-k accuracy
+    test_accuracy1 = num_correct_test_predictions_top1 / test_image_count * 100
+    test_accuracy2 = num_correct_test_predictions_top2 / test_image_count * 100
+    test_accuracy3 = num_correct_test_predictions_top3 / test_image_count * 100
+    test_accuracy5 = num_correct_test_predictions_top5 / test_image_count * 100
+
+    print(
+        "Test accuracy 1: {:.4f}, Test accuracy 2: {:.4f}, Test accuracy 3: {:.4f}, Test accuracy 5: {:.4f}".format(
+             test_accuracy1, test_accuracy2, test_accuracy3, test_accuracy5
+        )
+    )
+
+clip_module()
+
--- preprocess.py
+++ preprocess.py
@@ -7,10 +7,10 @@ import torchvision
 
 from torch.utils.data import DataLoader
 from torchvision.io.image import ImageReadMode
-
+import clip
 
 class ImageDataset(torch.utils.data.IterableDataset):
-    def __init__(self, directory_path) -> None:
+    def __init__(self, directory_path, clip_preprocessing=None) -> None:
         super(ImageDataset).__init__()
 
         self.directory_path = directory_path
@@ -26,6 +26,8 @@ class ImageDataset(torch.utils.data.IterableDataset):
         self.country_labels = list(sorted(set(countries)))
         self.label_map = {val: i for i, val in enumerate(self.country_labels)}
 
+        self.clip_preprocessing = clip_preprocessing
+
     def __iter__(self) -> None:
         # shuffle the image paths
         random.shuffle(self.image_paths)
@@ -36,9 +38,15 @@ class ImageDataset(torch.utils.data.IterableDataset):
             label = label[: label.rindex("_")]
             # return image as tensor and label
             try :
-                yield torchvision.io.read_image(
-                    os.path.join(self.directory_path, path), mode=ImageReadMode.RGB
-                ), label
+                if self.clip_preprocessing is not None:
+                    yield torchvision.io.read_image(
+                        os.path.join(self.directory_path, path), mode=ImageReadMode.RGB
+                    ), label
+
+                else:
+                    yield self.clip_preprocessing(torchvision.io.read_image(
+                        os.path.join(self.directory_path, path), mode=ImageReadMode.RGB
+                    )), label
             except RuntimeError as e:
                 print(path)
                 raise e
