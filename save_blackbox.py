48fb80e4aca53ab2f1a059c6b61cd4afbf8b342b
Abhyudaya Sharma <11471599+AbhyudayaSharma@users.noreply.github.com>
2023-05-03 10:57:12 -0400

    Save preprocessed image

--- save_blackbox.py
+++ save_blackbox.py
@@ -6,7 +6,7 @@ def main():
     img = torchvision.io.read_image(path=sys.argv[1], mode=torchvision.io.ImageReadMode.RGB)
     img[:, 16:72, 1257:1505] = 0
     img[:, 451:604, 1257:1505] = 0
-    torchvision.utils.save_image(img, path=sys.argv[2])
+    torchvision.utils.save_image(img, sys.argv[2])
 
 
 if __name__ == '__main__':
